15da09e52a558d75dbda85f4afd63bbf74c1351e
Alejo <alejo.rios@leapsight.com>
2018-11-03 13:20:45 -0300

    add garbage collector

--- config/config.exs
+++ config/config.exs
@@ -3,6 +3,7 @@
 use Mix.Config
 
 config :contacts, ecto_repos: [Contacts.Repo]
+config :contacts, garbage_collector_interval: 600_000
 # This configuration is loaded before any dependency and is restricted
 # to this project. If another project depends on this project, this
 # file won't be loaded nor affect the parent project. For this reason,
